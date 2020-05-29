98248ef4255cad328c4557434c57d371d6d85fe9
Orkie <55590370+popolinka@users.noreply.github.com>
2020-05-29 11:52:08 +0200

    Update recipeLinkMerger.py

--- recipe_parser/recipeLinkMerger.py
+++ recipe_parser/recipeLinkMerger.py
@@ -2,7 +2,7 @@ filenames = ['food52_links.txt', 'allRecipes_links.txt', 'chefKoch_Links.txt', '
              , 'essenUndTrinken_links.txt', 'kochBar_links.txt' , 'epicurious_links.txt',
              'xiachufang_links.txt', 'xinshipu_links.txt']
 
-with open('/Users/orkunkadioglu/PycharmProjects/recipeLinkCrawler/recipeLinks.txt', 'w') as outfile:
+with open('~/PATH, 'w') as outfile:
     for fname in filenames:
         with open(fname) as infile:
             outfile.write(infile.read())
